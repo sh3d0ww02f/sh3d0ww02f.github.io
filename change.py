85fdde6c5b01e082e9f5a895071cd63b5b35a808
sh3d0ww02f <95856600+sh3d0ww02f@users.noreply.github.com>
2022-02-01 15:31:07 +0800

    Update change.py

--- change.py
+++ change.py
@@ -1,1 +1,13 @@
-print("test")
+template='''
+<html>
+  <head>
+    <title>A Test For first step</title>
+  </head>
+  <body>
+  {}
+  </body>
+</html>
+'''
+content="test<br>"
+with open("index.html",'w')as f:
+    f.write(template.format(content))
